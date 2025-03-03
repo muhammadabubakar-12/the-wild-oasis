af8192d34f3c1935b8eb8c8cda0bd29ee84c5912
Muhammad Abu Bakar <abubakarkhan1222588@gmail.com>
2025-03-03 12:05:12 +0500

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 export const supabaseUrl = 'https://ygfbucaedbrizmotqyne.supabase.co';
 const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlnZmJ1Y2FlZGJyaXptb3RxeW5lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA4ODI5NzAsImV4cCI6MjA0NjQ1ODk3MH0.cK1FNyR0I4_eVMdJNgtxGcYwceZusT0JYH54IjcrJjE';
+  'this_key_is_hidden_by_author';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
